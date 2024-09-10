08a50a9f804d8aadcd53dadb5e1f2ec9a11779f1
000vince000 <00vince00@gmail.com>
2024-09-10 16:12:46 -0400

    big refactor: made schema creation modular and dynamically constructed

--- create-schema.py
+++ create-schema.py
@@ -1,57 +1,75 @@
 import duckdb
 import os
+import db_operations
+
+def create_table(conn, table_name, columns):
+    quoted_table_name = f'"{table_name}"'
+    create_table_query = f"""
+    CREATE TABLE IF NOT EXISTS {quoted_table_name} (
+        {', '.join(columns)}
+    )
+    """
+    try:
+        db_operations.execute_query(conn, create_table_query)
+        print(f"Table {table_name} created successfully")
+    except Exception as e:
+        print(f"An error occurred while creating {table_name}: {str(e)}")
+        raise
+
+def create_sequence(conn, table_name):
+    create_seq_query = f"CREATE SEQUENCE IF NOT EXISTS {table_name}_id_seq START 1;"
+    try:
+        conn.execute(create_seq_query)
+        print(f"Sequence {table_name}_id_seq created successfully")
+    except Exception as e:
+        print(f"An error occurred while creating sequence for {table_name}: {str(e)}")
+        raise
+
+def create_unique_index(conn, table_name, columns):
+    quoted_columns = ', '.join(f'"{col}"' for col in columns)
+    create_unique_index_query = f"""
+    CREATE UNIQUE INDEX unique_{table_name} ON {table_name} ({quoted_columns});
+    """
+    try:
+        conn.execute(create_unique_index_query)
+        print(f"Unique index unique_{table_name} created successfully")
+    except Exception as e:
+        print(f"An error occurred while creating unique index for {table_name}: {str(e)}")
+        raise
 
 # This function is idempotent
 def create_table_consolidated_transactions(conn):
-    try:
-        # Create autoincrementing sequence
-        create_seq_query = "CREATE SEQUENCE IF NOT EXISTS consolidated_transactions_id_seq START 1;"
-        conn.execute(create_seq_query)
+    table_name = "consolidated_transactions"
+    create_sequence(conn, table_name)
 
-        # Create table
-        create_table_query = """
-        CREATE TABLE IF NOT EXISTS consolidated_transactions (
-            id BIGINT DEFAULT nextval('consolidated_transactions_id_seq') PRIMARY KEY,
-            "Card" VARCHAR,
-            "Transaction Date" DATE,
-            "Description" VARCHAR,
-            "Category" VARCHAR,
-            "Type" VARCHAR,
-            "Amount" DECIMAL(10, 2),
-            "Memo" VARCHAR,
-            UNIQUE ("Card", "Transaction Date", "Description", "Amount")
-        )
-        """
-        conn.execute(create_table_query)
-        print("Table consolidated_transactions created successfully")
+    columns = [
+        f"id BIGINT DEFAULT nextval('{table_name}_id_seq') PRIMARY KEY",
+        '"Card" VARCHAR',
+        '"Transaction Date" DATE',
+        '"Description" VARCHAR',
+        '"Category" VARCHAR',
+        '"Type" VARCHAR',
+        '"Amount" DECIMAL(10, 2)',
+        '"Memo" VARCHAR'
+    ]
 
-    except Exception as e:
-        print(f"An error occurred while creating consolidated_transactions: {str(e)}")
-        raise
+    create_table(conn, table_name, columns)
+    create_unique_index(conn, table_name, ["Card", "Transaction Date", "Description", "Amount"])
+
 
 # This function is idempotent
 def create_table_category_budgets(conn):
-    try:
-        # Create autoincrementing sequence
-        create_seq_query = "CREATE SEQUENCE IF NOT EXISTS category_budgets_id_seq START 1;"
-        conn.execute(create_seq_query)
-
-        # Create table
-        create_table_query = """
-        CREATE TABLE IF NOT EXISTS category_budgets (
-            id BIGINT DEFAULT nextval('category_budgets_id_seq') PRIMARY KEY,
-            category VARCHAR,
-            budget INTEGER,
-            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (category) REFERENCES categories(category)
-        )
-        """
-        conn.execute(create_table_query)
-        print("Table category_budgets created successfully")
-
-    except Exception as e:
-        print(f"An error occurred while creating category_budgets: {str(e)}")
-        raise
+    table_name = "category_budgets"
+    create_sequence(conn, table_name)
+    columns = [
+        f"id BIGINT DEFAULT nextval('{table_name}_id_seq') PRIMARY KEY",
+        "category VARCHAR",
+        "budget INTEGER",
+        "timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP",
+        f"FOREIGN KEY (category) REFERENCES categories(category)"
+    ]
+    create_table(conn, table_name, columns)
+    create_unique_index(conn, table_name, ["category"])
 
 def create_current_budgets_view(conn):
     try:
@@ -72,72 +90,50 @@ def create_current_budgets_view(conn):
         raise
 
 def create_table_vendor_category_mapping(conn):
-    try:
-        # Create table
-        create_table_query = """
-        CREATE TABLE IF NOT EXISTS vendor_category_mapping (
-            vendor VARCHAR PRIMARY KEY,
-            category VARCHAR,
-            FOREIGN KEY (category) REFERENCES categories(category)
-        )
-        """
-        conn.execute(create_table_query)
-        print("Table vendor_category_mapping created successfully")
+    table_name = "vendor_category_mapping"
+    create_sequence(conn, table_name)
+    columns = [
+        f"id BIGINT DEFAULT nextval('{table_name}_id_seq') PRIMARY KEY",
+        "vendor VARCHAR",
+        "category VARCHAR",
+        f"FOREIGN KEY (category) REFERENCES categories(category)"
+    ]
+    create_table(conn, table_name, columns)
+    create_unique_index(conn, table_name, ["vendor"])
 
-    except Exception as e:
-        print(f"An error occurred while creating vendor_category_mapping: {str(e)}")
-        raise
+def create_table_surplus_and_deficit_breakdowns_and_items(conn):
+    table_name = "surplus_and_deficit_breakdowns"
+    create_sequence(conn, table_name)
+    columns = [
+        f"id BIGINT DEFAULT nextval('{table_name}_id_seq') PRIMARY KEY",
+        "description VARCHAR",
+        "breakdown JSON",
+        "effective_date DATE",
+        "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+    ]
+    create_table(conn, table_name, columns)
+    # need to verify if this works
+    create_unique_index(conn, table_name, ["description"])
+    create_table_surplus_and_deficit_breakdown_items(conn)
 
-def create_table_surplus_and_deficit_breakdowns(conn):
-    try:
-        # Create autoincrementing sequence
-        create_seq_query = "CREATE SEQUENCE IF NOT EXISTS surplus_and_deficit_breakdowns_id_seq START 1;"
-        conn.execute(create_seq_query)
-
-        # Create table
-        create_table_query = """
-        CREATE TABLE IF NOT EXISTS surplus_and_deficit_breakdowns (
-            id BIGINT DEFAULT nextval('surplus_and_deficit_breakdowns_id_seq') PRIMARY KEY,
-            description VARCHAR,
-            breakdown JSON,
-            effective_date DATE,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-        """
-        conn.execute(create_table_query)
-        print("Table surplus_and_deficit_breakdowns created successfully")
-        create_table_surplus_and_deficit_breakdown_items(conn)
-
-    except Exception as e:
-        print(f"An error occurred while creating surplus_and_deficit_breakdowns: {str(e)}")
-        raise
 
 def create_table_surplus_and_deficit_breakdown_items(conn):
-    try:
-        # Create autoincrementing sequence
-        create_seq_query = "CREATE SEQUENCE IF NOT EXISTS surplus_and_deficit_breakdown_items_id_seq START 1;"
-        conn.execute(create_seq_query)
-
-        # Create table
-        create_table_query = """
-        CREATE TABLE IF NOT EXISTS surplus_and_deficit_breakdown_items (
-            id BIGINT DEFAULT nextval('surplus_and_deficit_breakdown_items_id_seq') PRIMARY KEY,
-            surplus_and_deficit_breakdown_id BIGINT,
-            category VARCHAR,
-            description VARCHAR,
-            amount DECIMAL(10, 2),
-            date DATE,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (surplus_and_deficit_breakdown_id) REFERENCES surplus_and_deficit_breakdowns(id),
-            FOREIGN KEY (category) REFERENCES categories(category)
-        )
-        """
-        conn.execute(create_table_query)
-        print("Table surplus_and_deficit_breakdown_items created successfully")
-
-    except Exception as e:
-        print(f"An error occurred while creating surplus_and_deficit_breakdown_items: {str(e)}")
-        raise
+    table_name = "surplus_and_deficit_breakdown_items"
+    create_sequence(conn, table_name)
+    columns = [
+        f"id BIGINT DEFAULT nextval('{table_name}_id_seq') PRIMARY KEY",
+        "surplus_and_deficit_breakdown_id BIGINT",
+        "category VARCHAR",
+        "description VARCHAR",
+        "amount DECIMAL(10, 2)",
+        "date DATE",
+        "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP",
+        f"FOREIGN KEY (surplus_and_deficit_breakdown_id) REFERENCES surplus_and_deficit_breakdowns(id)",
+        f"FOREIGN KEY (category) REFERENCES categories(category)"
+    ]
+    create_table(conn, table_name, columns)
+    # need to verify if this works
+    create_unique_index(conn, table_name, ["surplus_and_deficit_breakdown_id", "category", "date"])
 
 def create_schema_menu(conn):
     while True:
@@ -160,7 +156,7 @@ def create_schema_menu(conn):
         elif choice == '4':
             create_table_vendor_category_mapping(conn)
         elif choice == '5':
-            create_table_surplus_and_deficit_breakdowns(conn)
+            create_table_surplus_and_deficit_breakdowns_and_items(conn)
         elif choice == '6':
             break
         else:
