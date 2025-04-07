e82fac3031e3f1a342d998c13b351d29fe6a41cc
Vincent Chen <00vince00@gmail.com>
2025-04-06 22:02:21 -0400

    added seeddata

--- populate-seeddata-into-duckdb.py
+++ populate-seeddata-into-duckdb.py
@@ -109,7 +109,9 @@ category_map = {
     "CLAUDE.AI": "Vince spending",
     "Google One": "Vince spending",
     "Google Storage": "Vince spending",
-    "Patreon": "Vince spending"
+    "Patreon": "Vince spending",
+    "APPLE.COM/BILL", "Kat spending",
+    "NYCDOT PARKING METERS", "Transportation"
 }
 populate_table(db_name, table_name, category_map.items(), ['keyword VARCHAR PRIMARY KEY', 'category VARCHAR'])
 
